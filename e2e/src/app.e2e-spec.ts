d524a2b573aa11d4d37ec99f50ed9d46d265d3c0
fede <federico.aineseder@intraway.com>
2021-08-08 14:25:29 -0300

    remove listing

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to listin-site!');
+    expect(page.getTitleText()).toEqual('Welcome to Mean!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'listin-site'`, () => {
+  it(`should have as title 'Mean'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('listin-site');
+    expect(app.title).toEqual('Mean');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to listin-site!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Mean-site!');
   });
 });
